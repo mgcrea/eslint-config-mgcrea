aa1e000b774afe8afa8b1026a8ee9cabec119bbc
Olivier Louvignes <olivier@mg-crea.com>
2016-01-22 14:52:20 +0100

    eslint(rule): add no-param-reassign rule

--- legacy.js
+++ legacy.js
@@ -7,6 +7,7 @@ module.exports = {
     "id-length": 0,
     "max-len": 0,
     "no-console": 0,
+    "no-param-reassign": [2, {"props": false}],
     "no-unused-vars": [2, {"args": "none"}],
     "no-use-before-define": [2, "nofunc"],
     "object-curly-spacing": [2, "never"],
