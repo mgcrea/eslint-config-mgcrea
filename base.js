18a0e5d5d684275694ed12643c98b4e8f449cd8a
Olivier Louvignes <olivier@mg-crea.com>
2016-02-22 15:55:08 +0200

    Revert "feat(base): add space-before-function-paren in rules"
    This reverts commit 81ef74a66dfd956a2b21cb62a89f2020214d1cc2.

--- base.js
+++ base.js
@@ -11,7 +11,6 @@ module.exports = {
     "no-param-reassign": [2, {"props": false}],
     "no-unused-vars": [2, {"args": "none"}],
     "no-use-before-define": [2, "nofunc"],
-    "object-curly-spacing": [2, "never"],
-    "space-before-function-paren": [2, "never"]
+    "object-curly-spacing": [2, "never"]
   }
 };
