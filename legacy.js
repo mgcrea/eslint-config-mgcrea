868ce4fd2ade8c0347b2f76951f034c62bc70d24
Olivier Louvignes <olivier@mg-crea.com>
2016-04-22 18:52:42 +0200

    feat(airbnb): add eslint tests, update deps

--- legacy.js
+++ legacy.js
@@ -1,21 +1,20 @@
 module.exports = {
-  'extends': [
+  extends: [
     'eslint-config-airbnb/legacy'
   ],
-  'rules': {
-    "comma-dangle": [2, "never"],
-    "id-length": 0,
-    "max-len": 0,
-    "no-console": 0,
-    "no-param-reassign": [2, {"props": false}],
-    "no-unused-vars": [2, {"args": "none"}],
-    "no-use-before-define": [2, "nofunc"],
-    "object-curly-spacing": [2, "never"],
-    "space-before-function-paren": [2, "never"],
-    "strict": [2, "global"],
-    "padded-blocks": 1,
-    "vars-on-top": 0,
-    "func-names": 0
+  rules: {
+    'comma-dangle': [2, 'never'],
+    'id-length': 0,
+    'max-len': 0,
+    'no-console': 0,
+    'no-param-reassign': [2, {props: false}],
+    'no-unused-vars': [2, {args: 'none'}],
+    'no-use-before-define': [2, 'nofunc'],
+    'object-curly-spacing': [2, 'never'],
+    'space-before-function-paren': [2, 'never'],
+    strict: [2, 'global'],
+    'padded-blocks': 1,
+    'vars-on-top': 0,
+    'func-names': 0
   }
 };
-
